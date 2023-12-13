3c58b57d0cfbb99248d10090de6429f7b42564d3
Your Name <you@example.com>
2023-12-13 17:25:55 -0500

    pushing to Node

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.use(express.static('media'));
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'media/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'views', 'index.html'));
+});
+
+app.post('/upload', upload.single('songFile'), (req, res) => {
+  res.redirect('/');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
