26852f0bb8c0446158ec63c83c4092e9a6b48a27
Your Name <you@example.com>
2023-12-15 13:36:58 -0500

    fixed an issue where it was redirecting to welcome screen even after you had visited it

--- welcome/welcome-script.js
+++ welcome/welcome-script.js
@@ -1,4 +1,8 @@
 function enterMusicPlayer() {
+    // Set the flag in local storage to indicate the user has visited the welcome screen
+    localStorage.setItem('visitedWelcomeScreen', true);
+  
+    console.log('Entering the music player');
     // Redirect to the main music player screen
     window.location.href = '../index.html';
   }
